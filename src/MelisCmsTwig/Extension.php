11129629e47b519b25dad48e714f9f28a8491509
ksuson <ksuson@melistechnology.com>
2019-09-12 19:04:28 +0800

    preg_match() is now available in twig templates

--- src/MelisCmsTwig/Extension.php
+++ src/MelisCmsTwig/Extension.php
@@ -30,6 +30,7 @@ class Extension extends AbstractExtension
     {
         return [
             new TwigFunction('xp', [$this, 'executePhp'], ['needs_context' => true, 'needs_environment' => true]),
+            new TwigFunction('pregMatch', [$this, 'pregMatch']),
         ];
     }
 
@@ -40,12 +41,52 @@ class Extension extends AbstractExtension
      * @param string $code
      * @return mixed
      */
-    public function executePhp(Twig_Environment $env, string $code)
+    public function executePhp(Twig_Environment $env, $context, string $code)
     {
         try {
             return eval($code);
         } catch (Exception $exception) {
             return $exception->getMessage();
+        }
+    }
+
+    /**
+     * preg_match (
+     *  string $pattern ,
+     *  string $subject
+     *  [array &$matches]
+     * ) : int
+     *
+     * RETURNS:
+     * If $pregMatches is NOT supplied (null):
+     *      - returns 1 if the pattern matches given subject, 0 if it does not, or FALSE if an error occurred.
+     *
+     * If $pregMatches is supplied (array):
+     *      - then it is filled with the results of search. $matches[0] will contain the text
+     *  that matched the full pattern, $matches[1] will have the text that matched the first captured
+     *  parenthesized subpattern, and so on.
+     *
+     *      Ex. {% set pregMatches = pregMatch('/<img\\s.*?\\bsrc="(.*?)".*?>/si', subject, {}) %}
+     *
+     * source: https://www.php.net/manual/en/function.preg-match.php
+     *
+     * Gotchas in regex pattern:
+     *  - Twig auto-escapes the \ character, fix it by adding another \\ (double escaping)
+     * https://craftcms.stackexchange.com/a/4271
+     *
+     * @param $context
+     * @param string $pattern
+     * @param string $subject
+     * @param null $pregMatches
+     * @return false|int
+     */
+    public function pregMatch(string $pattern, string $subject, $pregMatches = null)
+    {
+        if (is_array($pregMatches)) {
+            preg_match($pattern, $subject, $pregMatches);
+            return $pregMatches;
+        } else {
+            return preg_match($pattern, $subject);
         }
     }
 }
